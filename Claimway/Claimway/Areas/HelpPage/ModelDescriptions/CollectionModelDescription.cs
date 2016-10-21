a2a0b2c694d843153ab0c76098b92642ecd44346
olegp306 <olegp306@gmail.com>
2016-10-21 12:28:25 +0700

    initial statement. Empty ASP.NET WebAPi Individual indentification

---
+++ Claimway/Claimway/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Claimway.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Claimway/Claimway/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Claimway.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
